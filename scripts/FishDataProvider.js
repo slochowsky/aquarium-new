49f6584cff2a757a4e08aa89f3b5eb803a58a30c
blake slochowsky <blakeslochowsky.com>
2019-12-09 12:04:27 -0600

    redo

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,51 @@
+const fishCollection = [
+    {
+        name: "Bitey",
+        Species: "Piranha",
+        Location: "Brazil",
+        Length: 4,
+        Food: "Humans"
+    },
+    {
+        name: "Jawsie",
+        Species: "Shark",
+        Location: "South Africa",
+        Length: 264,
+        Food: "Seals"
+    },
+    {
+        name: "Sam",
+        Species: "Salmon",
+        Location: "Alaska",
+        Length: 26,
+        Food: "Anchovies"
+    },
+    {
+        name: "Mr. Dangles",
+        Species: "Angler Fish",
+        Location: "Mariana Trench",
+        Length: 12,
+        Food: "Minnows"
+
+    },
+    {
+        name: "Snappy",
+        Species: "Red Snapper",
+        Location: "Gulf of Mexico",
+        Length: 30,
+        Food: "Eels"
+
+    },
+    {
+        name: "Berry",
+        Species: "Barracuda",
+        Location: "Coral Reefs",
+        Length: 70,
+        Food: "Salmon"
+
+    }
+]
+
+export const useFish = () => {
+    return fishCollection
+}
